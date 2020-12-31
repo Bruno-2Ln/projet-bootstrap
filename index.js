5231a821666043de10868ad6760add2df0ef3e9d
Bruno Delaine <bruno-d-34@hotmail.fr>
2020-12-31 22:10:36 +0100

    Ajout des médias queries

---
+++ index.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+let year = document.getElementById('year')
+let body = document.getElementsByTagName('body')
+
+year.textContent = new Date().getFullYear();
+
+body.scrollspy({target: ''})
+})
